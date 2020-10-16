9ec737953778db373930e9722c6a24ce23fd8462
Kevin Viglucci <kviglucci@gmail.com>
2020-10-16 18:52:35 -0500

    fix test/HistoryPlugin

--- test/HistoryPlugin.js
+++ test/HistoryPlugin.js
@@ -24,13 +24,16 @@ describe('HistoryPlugin', () => {
             ]
         });
 
-        const response = await client.get('http://example.com/example-endpoint').json();
-        const response2 = await client.get('http://example.com/example-endpoint-1').json();
+        const response = await client.get('http://example.com/example-endpoint');
+        const json = await response.json();
+
+        const response2 = await client.get('http://example.com/example-endpoint-1');
+        const json2 = await response2.json();
 
         expect(historyContainer.length).to.equal(2);
 
-        expect((await historyContainer[0].json()).foo).to.equal(response.foo);
-        expect((await historyContainer[1].json()).foo).to.equal(response2.foo);
+        expect((await historyContainer[0].json()).foo).to.equal(json.foo);
+        expect((await historyContainer[1].json()).foo).to.equal(json2.foo);
 
         expect(scope.isDone(), 'Expected all nock interceptors to have been called.').to.be.true;
     });
